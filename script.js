c074e85979e07db8cc4b2098bf6d6719f936c4bc
shrikant2753 <shrikantrp.00@gmail.com>
2023-08-07 18:09:00 +0530

    added search and filter

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const topGrid = document.getElementById("top-grid");
 const topList = document.getElementById("top-list");
 const gridContainer = document.getElementById("grid");
 const listContainer = document.getElementById("list");
+let cryptoData = [];
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchData("grid");
+// });
+
+window.onload = function () {
+  fetchData('grid');
+};
 
 topGrid.addEventListener("click", function () {
   topGrid.classList.add("active");
   topList.classList.remove("active");
   gridContainer.style.display = "grid";
   listContainer.style.display = "none";
+  fetchData('grid');
 });
 
 topList.addEventListener("click", function () {
@@ -17,27 +27,34 @@ topList.addEventListener("click", function () {
   topGrid.classList.remove("active");
   listContainer.style.display = "flex";
   gridContainer.style.display = "none";
+  fetchData('list');
 });
 
-async function fetchData() {
+async function fetchData(view) {
   const url = base_url+`coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
   let response = await fetch(url);
   let data = await response.json();
-  console.log(data);
-  addDomOnGrid(data)
-  addDomOnList(data);
+  // console.log(data);
+  cryptoData = data
+  if (view === 'grid') {
+    addDomOnGrid(data);
+  } else if (view === 'list') {
+    addDomOnList(data);
+  }
 }
 
 async function trending(){
     let url = base_url+`search/trending`
     let response = await fetch(url)
     let data = await response.json();
-    console.log(data);
+    // console.log(data);
     addDomOnList(data)
 }
 
 function addDomOnGrid(data) {
+  // console.log('grid');
   let grid = document.getElementById("grid");
+  grid.innerHTML=''
 
   data.forEach((crypto) => {
     let card = document.createElement("div");
@@ -62,8 +79,11 @@ function addDomOnGrid(data) {
   });
   container.appendChild(grid);
 }
+
 function addDomOnList(data){
+  // console.log('list');
     let list = document.getElementById("list");
+    list.innerHTML=''
 
     data.forEach((crypto) => {
         let listf = document.createElement("div");
@@ -88,4 +108,52 @@ function addDomOnList(data){
     });
     container.appendChild(list);
 }
-fetchData();
+
+const searchBar = document.getElementById('searchbar');
+// console.log(searchBar);
+searchBar.addEventListener('input', (event) => {
+    const query = event.target.value;
+    searchCrypto(query);
+});
+
+function searchCrypto(query) {
+  const filteredCrypto = cryptoData.filter((cryptoData) =>
+      cryptoData.name.toLowerCase().includes(query.toLowerCase()) || 
+      cryptoData.symbol.toUpperCase().includes(query.toUpperCase())
+  );
+  if(topGrid.classList.contains("active"))
+    return addDomOnGrid(filteredCrypto);
+  else
+    return addDomOnList(filteredCrypto)
+}
+
+// Add click event listeners to the sort buttons
+const sortMktCapButton = document.getElementById("sortMktCap");
+const sortPercentageButton = document.getElementById("sortPercentage");
+
+sortMktCapButton.addEventListener("click", () => {
+  sortCryptoByMarketCapDescending(cryptoData);
+  updateCryptoDisplay();
+});
+
+sortPercentageButton.addEventListener("click", () => {
+  sortCryptoByPercentageDescending(cryptoData);
+  updateCryptoDisplay();
+});
+
+function sortCryptoByMarketCapDescending(data) {
+  data.sort((a, b) => b.market_cap - a.market_cap);
+}
+
+function sortCryptoByPercentageDescending(data) {
+  data.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
+}
+
+function updateCryptoDisplay() {
+  const view = topGrid.classList.contains("active") ? "grid" : "list";
+  if (view === 'grid') {
+    addDomOnGrid(cryptoData);
+  } else if (view === 'list') {
+    addDomOnList(cryptoData);
+  }
+}
